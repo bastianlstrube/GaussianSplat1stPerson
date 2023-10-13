8e73226ea9232e1fb00744bab809592e29ff18d8
Bastian Leonhardt Strube <hammedhaaret@gmail.com>
2023-10-13 16:20:14 +0200

    reduced to one scene

--- demo/js/scene/Scene.js
+++ demo/js/scene/Scene.js
@@ -6,9 +6,9 @@ export class Scene {
     }
 
     load() {
-        const viewer = new GaussianSplat3D.Viewer(null, [0, -1, -0.6], [-1, -4, 6], [0, 4, -0],  20);
+        const viewer = new GaussianSplat3D.Viewer(null, [0, -1, 0], [-3.3816, 1.96931, -1.71890], [0.60910, 1.42099, 2.02511], 30);
         viewer.init();
-        viewer.loadFile('assets/data/garden/garden.splat')
+        viewer.loadFile('assets/data/stump/tbilisi.ply') //stump.splat')
         .then(() => {
             viewer.start();
         });
